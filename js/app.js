b82c5ab24e4727a95b0fbbf17fb862dc20c9bf0b
criamico <criamico@users.noreply.github.com>
2016-08-24 13:10:48 +0100

    fixed css for mobile; modified infoWindow behaviour, now only one at the time can be opened, other small fixes

--- js/app.js
+++ js/app.js
@@ -53,13 +53,6 @@
     }
 
 
-    /*function executed when the user clicks on a marker*/
-     function openInfoWindow(e, selectedMarker){
-        e.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
-        $scope.map.panTo(selectedMarker.position);
-    };
-
     /*Initialization and customizations of google map */
     function initMap(){
         var mapDiv = document.getElementById('map'),
@@ -68,17 +61,17 @@
                 {
                     'coord': {lat: 37.525176, lng: 15.075818},
                     'image': 'img/pin-grey.png',
-                    'description': '<h3>Catania, Italy</h3><ul><li><img src="img/Università_di_Catania_logo.jpg"><h4>2007-2010 University of Catania</h4><p>Ma, Automation engineering</p></li><li><img src="img/Università_di_Catania_logo.jpg"><h4>2002-2006 University of Catania</h4><p>Ba, Electronic engineering</p></li></ul>',
+                    'description': '<h3>Catania, Italy</h3><ul><li><img src="img/Università_di_Catania_logo.jpg"><h4>2007-2010 </br> University of Catania</h4><p>M.Sc., Automation engineering</p></li><li><img src="img/Università_di_Catania_logo.jpg"><h4>2002-2006</br> University of Catania</h4><p>B.Sc., Electronic engineering</p></li></ul>',
                 },
                 {
                     'coord': {lat: 45.068133, lng: 7.683158},
                     'image': 'img/pin-grey.png',
-                    'description': '<h3>Torino, Italy</h3><ul><li><img src="img/Actia.jpg"><h4>2011-2012 ACTIA Italia S.r.L.</h4><p>Software Developer</p></li><li><img src="img/Cnh_industrial_logo.jpg"><h4>2010-2011 CNH Industrial (formerly Iveco S.p.A.)</h4><p>Electronic Systems Engineer</p></li></ul>',
+                    'description': '<h3>Torino, Italy</h3><ul><li><img src="img/Actia.jpg"><h4>2011-2012</br> ACTIA Italia S.r.L.</h4><p>Software Developer</p></li><li><img src="img/Cnh_industrial_logo.jpg"><h4>2010-2011</br> CNH Industrial</br>(formerly Iveco S.p.A.)</h4><p>Electronic Systems Engineer</p></li></ul>',
                 },
                 {
                     'coord': {lat: 42.655697, lng: -83.233414},
                     'image': 'img/pin-grey.png',
-                    'description': '<h3>Auburn Hills, MI, USA</h3><ul><li><img src="img/fca_logo.jpg"><h4>2013-2015 FCA North America (formerly Chrysler LLC)</h4><p>UI Software Developer</p></li></ul>',
+                    'description': '<h3>Auburn Hills, MI, USA</h3><ul><li><img src="img/fca_logo.jpg"><h4>2013-2015</br> FCA North America</h4><p>UI Software Developer</p></li></ul>',
 
                 },
                 {
@@ -91,7 +84,7 @@
 
             var zoomValue = 3;
             if (windowWidth < 768)
-                zoomValue = 2;
+                zoomValue = 1;
 
             var mapOptions = {
                 center: {lat: 39.914036, lng: -39.778708},
@@ -101,7 +94,18 @@
 
             map = new google.maps.Map(mapDiv, mapOptions);
 
+            var infoWindow = new google.maps.InfoWindow({
+                maxWidth: 320,
+            });
 
+            /*function needed to open the info window*/
+            function addInfoWindow(marker) {
+                    google.maps.event.addListener(marker, 'click', function(){
+                        infoWindow.open(map, marker);
+                        infoWindow.setContent(marker.content);
+                    });
+
+            };
 
         places.forEach(function createMarkers(item){
 
@@ -111,22 +115,26 @@
                 position: new google.maps.LatLng(item.coord),
                 name: item.name,
                 icon: item.image,
+                content: '<div class="infoWindowContent">' + item.description +'</div>'
 
             });
 
-
+            addInfoWindow(marker);
 
             /*create the info windows */
-            marker.infoWindow = new google.maps.InfoWindow({
+           /* var infoWindow = new google.maps.InfoWindow({
                 maxWidth: 320,
                 content: '<div class="infoWindowContent">' + item.description +'</div>'
             });
+*/
 
 
-
-            marker.addListener('click', function() {
+           /* marker.addListener('click', function() {
                 marker.infoWindow.open(map, marker);
-              });
+
+            });*/
+
+
 
         });
 
